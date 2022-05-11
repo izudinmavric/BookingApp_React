d2c5795b4bfb515f1ca6627ae179b71a8ce99494
izudinmavric <izudin.mavric@gmail.com>
2022-05-11 17:00:46 +0200

    homepage_footer

--- bookingapp/src/pages/list/List.jsx
+++ bookingapp/src/pages/list/List.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import "./list.css";
 
 const List = () => {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
